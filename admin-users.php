9eb0cf4255f6ae81b05385ffd95f7dc95bda2c1d
fabiosbrito <fabiosilbrito@gmail.com>
2024-02-01 13:28:07 -0300

    Ajustes na classe admin-users.php

--- admin-users.php
+++ admin-users.php
@@ -8,21 +8,21 @@ use \Hcode\Model\User;
 * Route to password user edit button. Via GET
 * New button user password edit.
 */
-$app->get("/admin/users/:iduser/password", function($iduser)
+$app->get( "/admin/users/:iduser/password", function( $iduser )
 {
 
 	User::verifyLogin();
 
 	$user = new User();
 
-	$user->get((int)$iduser);
+	$user->get(( int )$iduser );
 
 	$page = new PageAdmin();
 
-	$page->setTpl("users-password", [
-		"user"=>$user->getValues(),
-		"msgError"=>User::getError(),
-		"msgSuccess"=>User::getSuccess()
+	$page->setTpl( "users-password", [
+		"user" => $user->getValues(),
+		"msgError" => User::getError(),
+		"msgSuccess" => User::getSuccess()
 	]);
 
 });
@@ -31,7 +31,7 @@ $app->get("/admin/users/:iduser/password", function($iduser)
 * Route to password user edit button. Via POST
 * New button user password edit.
 */
-$app->post("/admin/users/:iduser/password", function($iduser)
+$app->post( "/admin/users/:iduser/password", function( $iduser )
 {
 
 	User::verifyLogin();
@@ -41,39 +41,39 @@ $app->post("/admin/users/:iduser/password", function($iduser)
 	 * If the password was entered. If the password field is not empty.
 	 * If the password confirmation is the same as the entered password.
 	 **/
-	if (!isset($_POST['despassword']) || $_POST['despassword']==='') {
+	if ( !isset( $_POST[ 'despassword' ]) || $_POST[ 'despassword' ]==='' ) {
 
-		User::setError("Preencha a nova senha.");
-		header("Location: /admin/users/$iduser/password");
+		User::setError( "Preencha a nova senha." );
+		header( "Location: /admin/users/$iduser/password" );
 		exit;
 
 	}
 
-	if (!isset($_POST['despassword-confirm']) || $_POST['despassword-confirm']==='') {
+	if ( !isset( $_POST[ 'despassword-confirm' ]) || $_POST[ 'despassword-confirm' ]==='' ) {
 
-		User::setError("Preencha a confirmação da nova senha.");
-		header("Location: /admin/users/$iduser/password");
+		User::setError( "Preencha a confirmação da nova senha." );
+		header( "Location: /admin/users/$iduser/password" );
 		exit;
 
 	}
 
-	if ($_POST['despassword'] !== $_POST['despassword-confirm']) {
+	if ( $_POST[ 'despassword' ] !== $_POST[ 'despassword-confirm' ]) {
 
-		User::setError("Confirme corretamente as senhas.");
-		header("Location: /admin/users/$iduser/password");
+		User::setError( "Confirme corretamente as senhas digitadas." );
+		header( "Location: /admin/users/$iduser/password" );
 		exit;
 
 	}
 
 	$user = new User();
 
-	$user->get((int)$iduser);
+	$user->get(( int )$iduser );
 
-	$user->setPassword(User::getPasswordHash($_POST['despassword']));
+	$user->setPassword( User::getPasswordHash( $_POST[ 'despassword' ]));
 
-	User::setSuccess("Senha alterada com sucesso.");
+	User::setSuccess( "Senha alterada com sucesso." );
 
-	header("Location: /admin/users/$iduser/password");
+	header( "Location: /admin/users/$iduser/password" );
 	exit;
 
 });
@@ -82,45 +82,45 @@ $app->post("/admin/users/:iduser/password", function($iduser)
 * Route for user search and pagination.
 * Searches for the user and limits the number  of records per page.
 */
-$app->get("/admin/users", function()
+$app->get( "/admin/users", function()
 {
 
 	User::verifyLogin();
 
-	$search = (isset($_GET['search'])) ? $_GET['search'] : "";
-	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+	$search = ( isset( $_GET[ 'search' ])) ? $_GET[ 'search' ] : "";
+	$page = ( isset( $_GET[ 'page' ])) ? ( int )$_GET[ 'page' ] : 1;
 
-	if ($search != '') {
+	if ( $search != '' ) {
 
-		$pagination = User::getPageSearch($search, $page);
+		$pagination = User::getPageSearch( $search, $page );
 
 	} else {
 
-		$pagination = User::getPage($page);
+		$pagination = User::getPage( $page );
 
 	}
 
 	$pages = [];
 
-	for ($x = 0; $x < $pagination['pages']; $x++)
+	for ( $x = 0; $x < $pagination[ 'pages' ]; $x++ )
 	{
 
-		array_push($pages, [
-			'href'=>'/admin/users?'.http_build_query([
-				'page'=>$x+1,
-				'search'=>$search
+		array_push( $pages, [
+			'href' => '/admin/users?'.http_build_query([
+				'page' => $x+1,
+				'search' => $search
 			]),
-			'text'=>$x+1
+			'text' => $x+1
 		]);
 
 	}
 
 	$page = new PageAdmin();
 
-	$page->setTpl("users", array(
-		"users"=>$pagination['data'],
-		"search"=>$search,
-		"pages"=>$pages
+	$page->setTpl( "users", array(
+		"users" => $pagination[ 'data' ],
+		"search" => $search,
+		"pages" => $pages
 	));
 
 });
@@ -128,32 +128,32 @@ $app->get("/admin/users", function()
 /*
 * Route to screen new user create.
 */
-$app->get("/admin/users/create", function()
+$app->get( "/admin/users/create", function()
 {
 
 	User::verifyLogin();
 
 	$page = new PageAdmin();
 
-	$page->setTpl("users-create");
+	$page->setTpl( "users-create" );
 
 });
 
 /*
 * Route to screen user delete.
 */
-$app->get("/admin/users/:iduser/delete", function($iduser)
+$app->get( "/admin/users/:iduser/delete", function( $iduser )
 {
 
 	User::verifyLogin();	
 
 	$user = new User();
 
-	$user->get((int)$iduser);
+	$user->get(( int )$iduser );
 
 	$user->delete();
 
-	header("Location: /admin/users");
+	header( "Location: /admin/users" );
 	exit;
 
 });
@@ -161,19 +161,19 @@ $app->get("/admin/users/:iduser/delete", function($iduser)
 /*
 * Route to screen user update/edit.
 */
-$app->get("/admin/users/:iduser", function($iduser)
+$app->get( "/admin/users/:iduser", function( $iduser )
 {
 
 	User::verifyLogin();
 
 	$user = new User();
 
-	$user->get((int)$iduser);
+	$user->get(( int )$iduser );
 
 	$page = new PageAdmin();
 
-	$page->setTpl("users-update", array(
-		"user"=>$user->getValues()
+	$page->setTpl( "users-update", array(
+		"user" => $user->getValues()
 	));
 
 });
@@ -181,22 +181,22 @@ $app->get("/admin/users/:iduser", function($iduser)
 /*
 * Route to screen new user create.
 */
-$app->post("/admin/users/create", function()
+$app->post( "/admin/users/create", function()
 {
 
 	User::verifyLogin();
 
 	$user = new User();
 
-	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+	$_POST[ "inadmin" ] = ( isset( $_POST[ "inadmin" ]))?1:0;
 
-	$_POST['despassword'] = User::getPasswordHash($_POST['despassword']);
+	$_POST[ 'despassword' ] = User::getPasswordHash( $_POST[ 'despassword' ]);
 
-	$user->setData($_POST);
+	$user->setData( $_POST );
 
 	$user->save();
 
-	header("Location: /admin/users");
+	header( "Location: /admin/users" );
 	exit;
 
 });
@@ -204,22 +204,22 @@ $app->post("/admin/users/create", function()
 /*
 * Route to screen user updating.
 */
-$app->post("/admin/users/:iduser", function($iduser)
+$app->post( "/admin/users/:iduser", function( $iduser )
 {
 
 	User::verifyLogin();
 
 	$user = new User();
 
-	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+	$_POST[ "inadmin" ] = ( isset( $_POST[ "inadmin" ]))?1:0;
 
-	$user->get((int)$iduser);
+	$user->get(( int )$iduser );
 
-	$user->setData($_POST);
+	$user->setData( $_POST );
 
 	$user->update();	
 
-	header("Location: /admin/users");
+	header( "Location: /admin/users" );
 	exit;
 
 });
