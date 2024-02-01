2662759867870e77810bc5cc0d3439f1e42b0b09
fabiosbrito <fabiosilbrito@gmail.com>
2024-02-01 11:45:04 -0300

    Update index.php
    index

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <?php 
 session_start();
 
-require_once("vendor/autoload.php");
+require_once( "vendor/autoload.php" );
 
 use \Slim\Slim;
 use \Hcode\Page;
 
 $app = new Slim();
 
-$app->config('debug', true);
+$app->config( 'debug', true );
 
-require_once("functions.php");
-require_once("site.php");
-require_once("admin.php");
-require_once("admin-users.php");
-require_once("admin-categories.php");
-require_once("admin-products.php");
-require_once("admin-orders.php");
+require_once( "functions.php" );
+require_once( "site.php" );
+require_once( "admin.php" );
+require_once( "admin-users.php" );
+require_once( "admin-categories.php" );
+require_once( "admin-products.php" );
+require_once( "admin-orders.php" );
 
 $app->run();
 
